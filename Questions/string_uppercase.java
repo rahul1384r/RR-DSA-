192b0f8c517d7686c8e18b09b5906c29a6eb230b
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-01-31 05:08:03 +0530

    string completed

---
+++ Questions/string_uppercase.java
@@ -0,0 +1,19 @@
+public class string_uppercase {
+    public static void main(String[] args) {
+        String str="hello we are rRY ";
+        StringBuilder sb=new StringBuilder("");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());  //converted StringBuilder to String.
+    }
+}
--- string_lec.java
+++ string_lec.java
@@ -1,13 +1,37 @@
 import java.util.Scanner;
 
+// public class string_lec {
+//     public static void main(String[] args) {
+//         // String str=new String("RRY");
+//         Scanner sc=new Scanner(System.in);
+//         String str1=sc.next();
+//         String str2=sc.nextLine();
+//         System.out.println(str1);
+//         System.out.println(str2);
+
+//     }
+    
+// }
+
+
+
 public class string_lec {
     public static void main(String[] args) {
         // String str=new String("RRY");
-        Scanner sc=new Scanner(System.in);
-        String str1=sc.next();
-        String str2=sc.nextLine();
-        System.out.println(str1);
-        System.out.println(str2);
+        
+        String str1="RRY is forever";
+        String str2="RRY";
+        String str3="";
+        // str1.equals(str2)
+        if(str1.equals(str2)){
+            System.out.println("true");
+        }
+        // str.substring(0,i)
+        // System.out.println(str1.substring(0,1));
+        for(int i=0;i<3;i++){
+            str3+=str1.charAt(i);
+        }
+        System.out.println(str3);
 
     }
     
