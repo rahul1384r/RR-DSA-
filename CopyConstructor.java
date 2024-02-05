3f41f7a95aa798b96651b3747a318f77a34cd2bf
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-06 03:59:53 +0530

    till constructor completed

--- CopyConstructor.java
+++ CopyConstructor.java
@@ -46,12 +46,21 @@ class Student{
     String name;
     int roll;
     int marks[];
-
+    // shallow copy
+    // Student(Student s1){
+    //     this.name=s1.name;
+    //     this.roll=s1.roll;
+    //     marks=new int[3];
+    //     this.marks=s1.marks;
+    // }
+    // // deep copy
     Student(Student s1){
+        marks=new int[3];
         this.name=s1.name;
         this.roll=s1.roll;
-        marks=new int[3];
-        this.marks=s1.marks;
+        for(int i=0;i<marks.length;i++){
+            this.marks[i]=s1.marks[i];
+        }
     }
     Student(){
         marks=new int[3];
