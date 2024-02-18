ad666b6bca30cefcea1409044aeb00e93ab2c741
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-18 12:54:35 +0530

    first occurence of an element in array

---
+++ Questions/recursion_7.java
@@ -0,0 +1,18 @@
+public class recursion_7 {
+
+    public static void firstOcc(int[] arr,int target,int i){
+        if(i==arr.length-1){
+            return;
+        }
+        if(arr[i]==target){
+            System.out.println(i);
+            return;
+        }
+        firstOcc(arr, target, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        int target=3;
+        firstOcc(arr, target, 0);
+    }
+}
