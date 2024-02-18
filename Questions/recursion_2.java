e400455dfe358fadf4fd6e249f867215e23b0651
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-02-18 10:55:05 +0530

    factorial of a program

--- Questions/recursion_2.java
+++ Questions/recursion_2.java
@@ -1,15 +1,34 @@
+// public class recursion_2 {
+
+//     public static void printInc(int n){
+//         if(n==10){
+//             System.out.print(n);
+//             return;
+//         }
+//         System.out.print(n+" ");
+//         printInc(n+1);
+//     }
+//     public static void main(String[] args) {
+//         int n=10;
+//         printInc(0);
+//     }
+// }
+
+
+
+// another method
 public class recursion_2 {
 
     public static void printInc(int n){
-        if(n==10){
-            System.out.print(n);
+        if(n==1){
+            System.out.print(n+" ");
             return;
         }
+        printInc(n-1);
         System.out.print(n+" ");
-        printInc(n+1);
     }
     public static void main(String[] args) {
         int n=10;
-        printInc(0);
+        printInc(n);
     }
 }
